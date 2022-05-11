d218682e408047825c638b96ead7dea6f81ad9dd
davec504 <dave@telicent.io>
2022-05-11 14:40:43 +0100

    added tests for TelicentGrid

---
+++ src/Grid/Grid.test.jsx
@@ -0,0 +1,102 @@
+import { screen, render } from "@testing-library/react";
+import TelicentGrid from ".";
+import AssetProvider from "../AssetContext";
+
+const assets = [
+  {
+    id: "fed",
+    uri: "http://fed.co/test-data/iow#W001",
+    name: "Federal Reserve",
+    gridIndex: 1,
+    scoreColour: "hsl(117.96610169491527,100%, 50%)",
+    countColour: "hsl(117.96610169491527,100%, 50%)",
+    count: 1,
+    criticality: 3,
+    onClick: jest.fn(),
+  },
+  {
+    id: "irs",
+    uri: "http://irs.co/test-data/iow#W002",
+    name: "Internal Revenue System",
+    gridIndex: 2,
+    scoreColour: "hsl(117.96610169491527,100%, 50%)",
+    countColour: "hsl(117.96610169491527,100%, 50%)",
+    count: 1,
+    criticality: 3,
+    onClick: jest.fn(),
+  },
+];
+
+const connections = [
+  {
+    category: "connection",
+    uri: "http://fed.co/iow#connector_W001_W002",
+    source: "http://fed.co/test-data/iow#W001",
+    sourceName: "Federal Reserve",
+    sourceLat: 40.5,
+    sourceLon: 20.3,
+    sourceScoreColour: undefined,
+    target: "http://irs.co/test-data/iow#W002",
+    targetLat: 40.3,
+    targetLon: 20.1,
+    targetName: "Internal Revenue Service",
+    targetScoreColour: undefined,
+    criticality: 3,
+    label: "W001-W002",
+  },
+];
+
+describe("Grid should populate assets and connections", () => {
+  describe("Assets should", () => {
+    beforeEach(() => {
+      render(
+        <AssetProvider>
+          <TelicentGrid assets={[assets[0]]} />
+        </AssetProvider>
+      );
+    });
+
+    it("populate first cell in row with asset id", () => {
+      const buttons = screen.getAllByRole("button", { name: /fed/i });
+      expect(buttons).toHaveLength(2);
+    });
+
+    it("show human readable name", () => {
+      const cells = screen.getAllByRole("cell", { name: /fed/i });
+      cells.forEach((cell) => {
+        expect(cell).toHaveTextContent(/federal reserve/i);
+      });
+    });
+  });
+
+  describe("connections should", () => {
+    beforeEach(() => {
+      render(
+        <AssetProvider>
+          <TelicentGrid connections={connections} assets={assets} />
+        </AssetProvider>
+      );
+    });
+
+    it("show criticality for both connections in the grid.", () => {
+      const buttons = screen.getAllByRole("button", { name: 3 });
+      expect(buttons).toHaveLength(2);
+    });
+  });
+
+  describe("when no data is passed grid should", () => {
+    beforeEach(() => {
+      render(
+        <AssetProvider>
+          <TelicentGrid />
+        </AssetProvider>
+      );
+    });
+    it("should not render any connections if none are passed", () => {
+      const cells = screen.queryAllByRole("cell");
+      const buttons = screen.queryAllByRole("button", { name: /3/ });
+      expect(buttons).toHaveLength(0);
+      expect(cells).toHaveLength(0);
+    });
+  });
+});
---
+++ src/Grid/index.jsx
@@ -0,0 +1,230 @@
+import React, { useContext } from "react";
+import { AssetContext } from "../AssetContext";
+import "./Grid.css";
+
+const TelicentGrid = ({ assets = [], connections = [] }) => {
+  const { onSelectedNode } = useContext(AssetContext);
+  const onClick = (type) => (e) => {
+    const { target } = e;
+    onSelectedNode(target.id, type);
+  };
+  const [zoom, setZoom] = React.useState(100);
+  const grid = `50px 22px 22px 106px repeat(${assets.length}, 22px)`;
+  const renderAssets = () => {
+    const assetGrid = assets.map((asset) => (
+      <AssetGrid
+        id={asset.id}
+        uri={asset.uri}
+        name={asset.name}
+        key={`asset-grid-${asset.uri}`}
+        gridIndex={asset.gridIndex}
+        scoreColour={asset.scoreColour}
+        countColour={asset.countColour}
+        count={asset.count}
+        criticality={asset.criticality}
+        onClick={onClick("asset")}
+      />
+    ));
+    const connectionsGrid = connections.map((connection) => (
+      <ConnectionGrid
+        uri={connection.uri}
+        key={`connection-${connection.uri}`}
+        criticality={connection.criticality}
+        source={assets.find((asset) => asset.uri === connection.source)}
+        target={assets.find((asset) => asset.uri === connection.target)}
+        onClick={onClick("connection")}
+      />
+    ));
+    return [...assetGrid, ...connectionsGrid];
+  };
+
+  return (
+    <div
+      className="h-full w-full"
+      style={{
+        position: "relative",
+        // height: "calc(100% - 24px)",
+        overflow: "auto",
+        marginBottom: "12px",
+        marginRight: "12px",
+      }}
+    >
+      <div style={{ width: "inherit" }}>
+        <div
+          style={{
+            zoom: `${JSON.stringify(zoom)}%`,
+            gridTemplateColumns: grid,
+          }}
+          className="main-grid"
+        >
+          {renderAssets()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AssetGrid = ({
+  id,
+  uri,
+  name,
+  count,
+  criticality,
+  countColour,
+  scoreColour,
+  gridIndex,
+  onClick,
+}) => {
+  return (
+    <>
+      <div
+        className="asset-id"
+        title={name}
+        style={{
+          position: "sticky",
+          left: 0,
+          backgroundColor: "black",
+          width: "50px",
+          gridColumnStart: 1,
+          gridRowStart: gridIndex + 1,
+          gridColumnEnd: 1,
+          gridRowEnd: gridIndex + 1,
+        }}
+        onClick={onClick}
+        id={`${uri}`}
+        role="button"
+      >
+        {id}
+      </div>
+      <div
+        title={name}
+        id={id}
+        role="cell"
+        className="asset-id"
+        style={{
+          position: "sticky",
+          left: "94px",
+          backgroundColor: "black",
+          overflow: "hidden",
+          gridColumnStart: 4,
+          gridRowStart: gridIndex + 1,
+          gridColumnEnd: 4,
+          gridRowEnd: gridIndex + 1,
+        }}
+      >
+        {name}
+      </div>
+      <div
+        title={name}
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 99,
+          backgroundColor: "black",
+          gridColumnStart: gridIndex + 4,
+          gridRowStart: 1,
+          gridColumnEnd: gridIndex + 4,
+          gridRowEnd: 1,
+        }}
+        className="col-header"
+        onClick={onClick}
+        id={`${uri}`}
+        role="button"
+      >
+        {id}
+      </div>
+
+      <div
+        className="blank-entry"
+        role="cell"
+        style={{
+          gridColumnStart: gridIndex + 4,
+          gridRowStart: gridIndex + 1,
+          gridColumnEnd: gridIndex + 4,
+          gridRowEnd: gridIndex + 1,
+        }}
+      />
+      <div
+        className="asset-id"
+        role="cell"
+        style={{
+          backgroundColor: countColour,
+          position: "sticky",
+          left: "50px",
+          overflow: "hidden",
+          gridColumnStart: 2,
+          gridRowStart: gridIndex + 1,
+          gridColumnEnd: 2,
+          gridRowEnd: gridIndex + 1,
+        }}
+      >
+        {count}
+      </div>
+      <div
+        className="asset-id"
+        role="cell"
+        style={{
+          backgroundColor: scoreColour,
+          position: "sticky",
+          left: "72px",
+          overflow: "hidden",
+          gridColumnStart: 3,
+          gridRowStart: gridIndex + 1,
+          gridColumnEnd: 3,
+          gridRowEnd: gridIndex + 1,
+        }}
+      >
+        {criticality}
+      </div>
+    </>
+  );
+};
+
+const ConnectionGrid = ({ uri, criticality, source, target, onClick }) => {
+  if (!source || !target) {
+    return null;
+  }
+  const x = source.gridIndex;
+  const y = target.gridIndex;
+
+  const colsInColumnHeader = 4;
+  const colsInRowHeader = 1;
+
+  const calculateSourceGridPosition = (x, y) => ({
+    gridColumnStart: colsInColumnHeader + x,
+    gridColumnEnd: colsInColumnHeader + x,
+    gridRowStart: colsInRowHeader + y,
+    gridRowEnd: colsInRowHeader + y,
+  });
+
+  const calculateTargetGridPosition = (x, y) => ({
+    gridColumnStart: colsInColumnHeader + y,
+    gridColumnEnd: colsInColumnHeader + y,
+    gridRowStart: colsInRowHeader + x,
+    gridRowEnd: colsInRowHeader + x,
+  });
+
+  return (
+    <>
+      <div
+        className={`grid-entry grid-entry-${criticality}`}
+        style={calculateSourceGridPosition(x, y)}
+        onClick={onClick}
+        id={`${uri}`}
+        role="button"
+      >
+        {criticality}
+      </div>
+      <div
+        className={`grid-entry grid-entry-${criticality}`}
+        style={calculateTargetGridPosition(x, y)}
+        onClick={onClick}
+        id={`${uri}`}
+        role="button"
+      >
+        {criticality}
+      </div>
+    </>
+  );
+};
+export default TelicentGrid;
