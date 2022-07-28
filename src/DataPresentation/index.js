ee7c968786e25d8061cd0bb1c61235861abe7975
davec504 <dave@telicent.io>
2022-07-28 15:07:06 +0200

    Removed duplicated logic
    Source and Target Uri was already set on the connection in the class constructor so there was no need to still have these checks

--- src/DataPresentation/index.js
+++ src/DataPresentation/index.js
@@ -4,40 +4,19 @@ import Details from "../Details";
 import { AssetContext } from "../AssetContext";
 import { ElementsContext } from "../ElementsContext";
 import TelicentMemoMap from "../Map/TelicentMap";
+import { findElement, getSelectedElement } from "./utils";
+
 const DataPresentation = () => {
   const { selected, type } = useContext(AssetContext);
   const { assetsRef, connectionsRef } = useContext(ElementsContext);
   const [element, setElement] = useState({});
 
-  const findElement = (elements, criteria) =>
-    elements.find((element) => element.uri === criteria);
-
   useEffect(() => {
-    let element;
-    if (type === "asset") {
-      element = findElement(assetsRef.current, selected);
-    } else if (type === "connection") {
-      element = findElement(connectionsRef.current, selected);
-      // TODO: check if uri exists on result
-      const source = findElement(assetsRef.current, element.source);
-      const target = findElement(assetsRef.current, element.target);
-      if (!source || !source.uri) {
-        console.warn(
-          `Source ${element.uri} element has no source or source uri does not exist`
-        );
-      } else {
-        element.source = source.uri;
-      }
-
-      if (!target || !target.uri) {
-        console.warn(
-          `Target ${element.uri} element has no target or target uri does not exist`
-        );
-      } else {
-        element.target = target.uri;
-      }
-    }
-    setElement(element);
+    const selectedElement =
+      type === "asset"
+        ? findElement(assetsRef.current, selected)
+        : findElement(connectionsRef.current, selected);
+    setElement(selectedElement);
   }, [selected, type, assetsRef, connectionsRef, setElement]);
 
   return (
