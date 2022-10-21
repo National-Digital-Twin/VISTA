4e5fb3c23d79c1d24abc4a8b29d0cf97613d0280
Alecsandria Missangu <alecsandria@telicent.io>
2022-10-21 16:42:20 +0100

    refactor: added back config check

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import React, { useState } from "react";
 import { StandardLayout } from "@telicent-io/ds";
 import "../node_modules/@telicent-io/ds/dist/style.css";
 
-import { Categories, Grid, InfoPanel, NetworkGraph, SponsorsLogos, TelicentMap } from "./components";
+import {
+  Categories,
+  Grid,
+  InfoPanel,
+  NetworkGraph,
+  SponsorsLogos,
+  TelicentMap,
+} from "./components";
 import { CytoscapeProvider, ElementsProvider } from "./context";
-// import config from "./config/app-config";
+import config from "./config/app-config";
 
 const App = () => {
-  /*   
-  const isConfigValid = (config) => config && typeof config === "object" && config.api && config.api.url;
-  if (!isConfigValid(config)) {
-    return <ErrorHandler message="Invalid config." />;
-  } */
-
   const [showGrid, setShowGrid] = useState(false);
 
   const toggleView = () => {
     setShowGrid((prevShow) => !prevShow);
   };
+  
+  if (config && config.api && config.api.url) {
+    console.error("Missing configuration");
+  }
 
   return (
     <StandardLayout appName="paralog" beta={true}>
