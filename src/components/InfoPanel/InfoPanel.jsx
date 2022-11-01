8c0f21e431261075398e8ffba3bfa0152bd14929
Vanessa Kemp <vanessa.kemp@telicent.io>
2022-11-01 12:29:54 +0000

    feat: wip - fixed the css

--- src/components/InfoPanel/InfoPanel.jsx
+++ src/components/InfoPanel/InfoPanel.jsx
@@ -5,6 +5,7 @@ import { kebabCase } from "lodash";
 import { ReactComponent as GoogleMapIcon } from "./assets/google-map-icon.svg";
 import { useEffect } from "react";
 import VerticalDivider from "../../lib/VerticalDivider";
+import classNames from "classnames";
 
 const viewType = {
   hasSingleItem: "singleItem",
@@ -41,63 +42,98 @@ const InfoPanel = () => {
     setSelected(selectedDetails);
   };
 
-  if (!expand) {
-    return (
-      <div className="absolute top-0 right-0 z-10 bg-black-200 p-3  gap-y-2 ">
-        <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
-      </div>
-    );
-  }
   return (
     <div
-      className="absolute top-0 right-0 z-10 bg-black-200 p-3 gap-y-2 w-2/5 overflow-y-auto"
-      style={{ minHeight: "50%", height: "50%" }}
+      className={classNames("absolute right-1 top-1 bg-black-200 z-10 rounded-md", {
+        "w-5/12 h-2/6": expand,
+      })}
     >
-      <div className=" z-10 flex items-center justify-end border-b border-black-500 gap-x-2 ">
-        <Toolbar selectedDetails={selectedDetails} selected={selected} onBack={handleBackButton} view={view} />
-        <VerticalDivider />
-        <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
-      </div>
-      <div className="overflow-y-auto">
-        <SelectedElements selected={selected} handleViewSelected={handleViewSelected} view={view} />
-      </div>
+      <Toolbar
+        selectedDetails={selectedDetails}
+        selected={selected}
+        onBack={handleBackButton}
+        view={view}
+        expand={expand}
+        toggleView={toggleView}
+      />
+      {expand && (
+        <div className="overflow-y-auto h-full px-2 bg-black-200 rounded-md">
+          <div className="relative ">
+            <SelectedElements selected={selected} handleViewSelected={handleViewSelected} view={view} />
+          </div>
+        </div>
+      )}
     </div>
   );
+
+  //   if (!expand) {
+  //     return (
+  //       <div className="absolute top-0 right-0 z-10 bg-black-200 p-3  gap-y-2 ">
+  //         <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
+  //       </div>
+  //     );
+  //   }
+  //   return (
+  //     <div
+  //       className="absolute top-0 right-0 z-10 bg-black-200 p-3 gap-y-2 w-2/5 overflow-y-auto"
+  //       style={{ minHeight: "50%", height: "50%" }}
+  //     >
+  //       <div className=" z-10 flex items-center justify-end border-b border-black-500 gap-x-2 ">
+  //         <Toolbar selectedDetails={selectedDetails} selected={selected} onBack={handleBackButton} view={view} />
+  //         <VerticalDivider />
+  //         <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
+  //       </div>
+  //       <div className="overflow-y-auto">
+  //         <SelectedElements selected={selected} handleViewSelected={handleViewSelected} view={view} />
+  //       </div>
+  //     </div>
+  //   );
 };
 
 const Toolbar = ({ selected, onBack, selectedDetails, view, toggleView, expand }) => {
-  if (view === "singleItem" && selectedDetails.length > 1) {
+  if (view === "singleItem" && selectedDetails.length > 1 && expand) {
     return (
-      <div className="flex items-center justify-between">
-        <button onClick={onBack} className="flex items-center gap-x-1 mr-auto">
+      <div className="flex items-center justify-between px-4 py-2 border-b border-black-500">
+        <button onClick={onBack} className="flex items-center font-medium text-lg">
           <span role="img" className="ri-arrow-left-s-line" />
           view all selected
         </button>
-        <StreetView latitude={selected.lat} longitude={selected.lng} />
-      </div>
-    );
-  }
-
-  if (view === "singleItem") {
-    return (
-      <div className="flex items-center justify-around">
-        <h2 className="font-medium text-lg">Element Details</h2>
-        <div className="flex items-center">
+        <div className="flex items-center gap-x-2">
           <StreetView latitude={selected.lat} longitude={selected.lng} />
+          <VerticalDivider />
+          <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
         </div>
       </div>
     );
   }
-  return <h2 className="font-medium text-lg">Element Details</h2>;
+
+  if (view === "singleItem" && expand) {
+    return (
+      <div className="flex items-center justify-between px-4 py-2 border-b border-black-500">
+        <h2 className="font-medium text-lg">Element Details</h2>
+        <div className="flex items-center gap-x-2">
+          <StreetView latitude={selected.lat} longitude={selected.lng} />
+          <VerticalDivider />
+          <ExpandButton selected={selectedDetails} view={view} toggleView={toggleView} expand={expand} />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className={classNames("flex items-center justify-between px-4 py-2", { "border-b border-black-500": expand })}>
+      {expand && <h2 className="font-medium text-lg">Selected Elements</h2>}
+      <ExpandButton selected={selectedDetails} toggleView={toggleView} expand={expand} />
+    </div>
+  );
 };
 
 const ExpandButton = ({ selected, toggleView, expand }) => {
-  const label = `${!expand ? "close" : "open"} details panel`;
+  const label = `${!expand ? "open" : "close"} details panel`;
   return (
     <div className="relative flex justify-end">
       <button aria-labelledby={kebabCase(label)} className="relative" onClick={toggleView}>
         <i className="ri-information-line text-[color:var(--app-Colour)] !text-2xl" />
-        {selected.length > 0 && (
+        {selected.length > 0 && !expand && (
           <span className="absolute -top-1.5 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-whiteSmoke-300 text-black-200 text-sm">
             {selected.length}
           </span>
@@ -127,7 +163,10 @@ const StreetView = ({ latitude, longitude }) => {
         rel="noreferrer"
         className="link"
       >
-        <GoogleMapIcon />
+        <div className="flex items-center">
+          {" "}
+          <GoogleMapIcon /> StreetView
+        </div>
       </a>
       <div className="linkBorder" />
     </div>
