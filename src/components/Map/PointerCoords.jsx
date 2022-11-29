6b3e83c7136b1b340fb1a3490c4c0d8d35970345
Alecsandria Missangu <alecsandria@telicent.io>
2022-11-29 16:17:14 +0000

    style: reduced padding

--- src/components/Map/PointerCoords.jsx
+++ src/components/Map/PointerCoords.jsx
@@ -4,8 +4,8 @@ const PointerCoordinates = ({ show, lat, lng }) => {
   if (!show || (!lat && !lng)) return null;
   return (
     <div className="absolute left-0 bg-black-200 p-1 flex items-center gap-x-2">
-      <div className="uppercase border w-fit py-0.5 px-2">lat, lng</div>
-      <p>
+      <div className="uppercase border w-fit px-2">lat, lng</div>
+      <p className="text-xs">
         {lat}, {lng}
       </p>
     </div>
