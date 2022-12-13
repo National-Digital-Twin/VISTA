4ad11aea2f715f981b6d0b27e994406e20712e19
Alecsandria Missangu <alecsandria@telicent.io>
2022-12-13 17:59:53 +0000

    chore: removed unused files
    re #190

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,2 @@
-export { default as AssetOld } from "./AssetOld";
 export { default as Asset } from "./Asset";
-export { default as Connection } from "./Connection";
 export { default as Dependency } from "./Dependency";
