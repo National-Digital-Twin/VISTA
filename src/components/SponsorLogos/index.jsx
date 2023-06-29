237e4e88c4bdcb097c57114a83c7f5e4ab95f0a5
Alecsandria Missangu <alecsandria@telicent.io>
2023-06-29 10:50:43 +0100

    refactor: pulling

--- src/components/SponsorLogos/index.jsx
+++ src/components/SponsorLogos/index.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import BEIS from "./assets/beis-standard-white-logo.png";
+import BEIS from "./assets/beis-logo.png";
 import RoyalEng from "./assets/Royal_Engineers_badge.png";
 import "./sponsor-logos.css";
 
 const SponsorLogos = () => {
   const small = 38;
-  const medium = 80;
+  const medium = 180;
   return (
     <div className="sponsor-logos">
-      <img src={BEIS} height={small} width={medium} alt="beis logo" />
-      <img
-        src={RoyalEng}
-        height={small}
-        width={small}
-        alt="royal engineers logo"
-      />
+      <img src={BEIS} width={medium} alt="beis logo" />
+      <img src={RoyalEng} width={small} alt="royal engineers logo" />
     </div>
   );
 };
