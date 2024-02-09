0760e5e50ac5752ca25b9fbbb973a373e0c7cdd7
Alecsandria Missangu <alecsandria@telicent.io>
2024-02-09 18:00:08 +0000

    refactor(handlers.js): removed ontology/styles handler from msw mocks

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -21,6 +21,5 @@ export const handlers = [
   rest.get(createParalogEndpoint("flood-watch-areas/polygon"), resolvers.floodAreaPolygons),
   rest.get(createParalogEndpoint("person/residences"), resolvers.personResidences),
   rest.get(createParalogEndpoint("ontology/class"), resolvers.ontologyClass),
-  rest.get(createOntologyEndpoint("styles/class"), resolvers.iconStyles),
   rest.get(createOntologyEndpoint("ontology/query"), resolvers.ontologyStyles),
 ];
