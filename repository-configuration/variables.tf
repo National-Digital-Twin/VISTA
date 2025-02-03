0e374f8d49d39b75c5ac2ff8a9647ec73701f63b
cruddasj <james.cruddas@informed.com>
2025-02-03 09:00:23 +0000

    [DPAV-152] Add repository configuration assets and PR template (#69)
    * [DPAV-152] Add OpenTofu repository configuration assets
    
    * [DPAV-152] Minor README adjustment
    
    * [DPAV-152] Link correction in README file
    
    * [DPAV-152] Add autolink solution
    
    * [DPAV-152] Add PR template
    
    * [DPAV-152] Fix for pre-commit checks
    
    * [DPAV-152] Exclude deploy directory from pre-commit checks
    
    * [DPAV-152] Remove tfstate from source
    
    * [DPAV-152] Add tfstate to gitignore
    
    * [DPAV-152] Final cleanup

---
+++ repository-configuration/variables.tf
@@ -0,0 +1,28 @@
+variable "token" {
+  description = "GitHub personal access token."
+  type        = string
+  sensitive   = true
+}
+
+variable "organisation" {
+  description = "The GitHub organisation name."
+  type        = string
+  default     = "National-Digital-Twin"
+}
+
+variable "repository_description" {
+  description = "GitHub repository description."
+  type        = string
+  default     = "Paralog is a geospatial visualisation tool for exploring and analysing data through a map interface. Originally developed by Telicent, it has been significantly expanded and developed by Coefficient. Paralog supports the visualisation of spatial relationships and enables data-driven decision-making across various sectors."
+}
+
+variable "requirement_tracking_url_base" {
+  description = "Requirement tracking system URL base to be used for autolinking commit messages."
+  type        = string
+}
+
+variable "requirement_tracking_id" {
+  description = "Requirement identifier to be used for autolinking commit messages. This ID should match those which prefix issue identifiers, for example DPAV."
+  type        = string
+  default     = "DPAV"
+}
