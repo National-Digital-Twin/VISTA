bd08a029692723c8d50d8485afbc452473a5b329
kam-is <kamran.mirza@informed.com>
2025-02-14 16:32:10 +0000

    feat: [DPAV-345] Add a header consistent with UDL

--- frontend/src/components/PageHeader/index.tsx
+++ frontend/src/components/PageHeader/index.tsx
@@ -1,45 +1,27 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Grid,
-  withStyles,
-} from "@material-ui/core";
+import { AppBar, Toolbar, Grid, withStyles } from "@material-ui/core";
 
 const styles = (theme) => ({
   appBar: {
-    backgroundColor: "#333333", // Custom background color for AppBar
+    backgroundColor: "#002244", // Custom background color for AppBar
   },
   logo: {
-    width: 40,
-    height: 40,
+    width: 200,
+    height: 100,
   },
   text: {
-    color: "#ffffff", // Custom text color
-    marginLeft: theme.spacing(1), // Adds space between logo and text
+    color: "#f0f2f2", // Custom text color
   },
 });
 
-const PageHeader = ({ classes }) => {
+const PageHeader = ({ appName, classes }) => {
   return (
     <AppBar position="static" className={classes.appBar}>
       <Toolbar>
         <Grid container alignItems="center">
           {/* Logo on the far left */}
           <Grid item>
-            <img
-              src="path-to-your-logo.png"
-              alt="Logo"
-              className={classes.logo}
-            />
-          </Grid>
-
-          {/* Text next to logo */}
-          <Grid item>
-            <Typography variant="h6" className={classes.text}>
-              Your Text Here
-            </Typography>
+            <img src="/logo.svg" alt="Logo" className={classes.logo} />
           </Grid>
         </Grid>
       </Toolbar>
