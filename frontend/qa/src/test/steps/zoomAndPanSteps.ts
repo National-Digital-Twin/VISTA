604fc0144c4b77aef0b2513a94bc83b1a4b1d2f9
adebalo.fisayo <adebalo.fisayo@informed.com>
2025-03-10 12:15:01 +0000

    steps for zoom

--- frontend/qa/src/test/steps/zoomAndPanSteps.ts
+++ frontend/qa/src/test/steps/zoomAndPanSteps.ts
@@ -13,34 +13,34 @@ When("I zoom in and out using the zoom button", async function () {
 });
 
 Then(
-  "I should be able to compare zoom in and out screenshots",
+  "I am zoomed in and out",
   async function () {
     layersPage = new LayersPage(basePage.page);
     await layersPage.zoomWithButtonWithScreenshotComparison();
   },
 );
 
-When("I zoom in and out on the map", async function () {
+When("I zoom in and out on the map using the mouse wheel", async function () {
   layersPage = new LayersPage(basePage.page);
   await layersPage.zoomOnTheMap();
   await layersPage.zoomOutTheMap();
 });
 
 Then(
-  "I should be able to see the zoom difference in the screenshots",
+  "I should be able to zoom in and out",
   async function () {
     layersPage = new LayersPage(basePage.page);
     await layersPage.zoomOnTheMapWithScreenshotComparison();
   },
 );
 
-When("I pan around the map", async function () {
+When("I am on the map", async function () {
   layersPage = new LayersPage(basePage.page);
   await layersPage.panAroundTheMap();
 });
 
 Then(
-  "I should be able to compare the screenshots before and after panning",
+  "I should be able to pan around the map",
   async function () {
     layersPage = new LayersPage(basePage.page);
     await layersPage.panWithScreenshotComparison();
