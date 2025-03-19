e386f80f5da0719046184ba7609cda5d3891c980
manoj.bisht <manoj.bisht@informed.com>
2025-03-19 17:19:06 +0000

    DPAV-679: Add unit test AppBodyLoadedContent

--- frontend/src/components/AppBody/AppBodyLoadedContents.test.tsx
+++ frontend/src/components/AppBody/AppBodyLoadedContents.test.tsx
@@ -1,14 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import AppBodyLoadedContents from "./AppBodyLoadedContents";
-
 // Mocking ElementsProvider
-jest.mock("@/context/ElementContext", () => ({
+jest.mock("../../context/ElementContext", () => ({
   ElementsProvider: ({ children }: { children: React.ReactNode }) => <div data-testid="elements-provider">{children}</div>,
 }));
 
 // Mocking ParalogMap component
-jest.mock("@/components/Map/ParalogMap", () => () => <div data-testid="paralog-map">Paralog Map</div>);
+jest.mock("../Map/ParalogMap", () => () => <div data-testid="paralog-map">Paralog Map</div>);
 
 describe("AppBodyLoadedContents Component", () => {
   it("should render ElementsProvider and ParalogMap", () => {
