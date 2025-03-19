3f23da1b12d9fb75cfa7278f5df670f7272b91f9
manoj.bisht <manoj.bisht@informed.com>
2025-03-19 15:48:46 +0000

    Add Jest set-up

---
+++ frontend/jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    '^.+\\.svg$': 'jest-transformer-svg'
+  },
+  moduleNameMapper: {
+    '^common/(.*)$': '<rootDir>/../common/$1',
+    '^(.*).svg\\?react$': '$1.svg'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
+};
