6ba0497b67e3026f5a2910469292dfb9ea6afaf6
DavidBuckley-Informed <david.buckley@informed.com>
2025-03-19 07:51:09 +0000

    Feature/[dpav-667] unable to select asset details (#194)
    * fix: [DPAV-667] - element details click handler
    
    * fix: [DPAV-667] - element details click handler
    
    * fix: [DPAV-667] - element details click handler

--- frontend/src/tools/AssetDetails/InfoPanel/SelectedElements/ElementDetails.tsx
+++ frontend/src/tools/AssetDetails/InfoPanel/SelectedElements/ElementDetails.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import {
   Card,
@@ -24,8 +23,8 @@ export interface ElementDefaultsProps {
 
 export default function ElementDetails({
   element,
-  showConnectedAssets,
   setConnectedAssetData,
+  showConnectedAssets,
 }: Readonly<ElementDefaultsProps>) {
   const elemIsAsset = isAsset(element);
 
@@ -38,13 +37,13 @@ export default function ElementDetails({
   const isLoading = assetInfo.isLoading;
   const isError = assetInfo.isError;
 
-  useEffect(() => {
+  const onClick = () => {
     if (elemIsAsset && assetInfo.data) {
       const details = element?.getDetails?.(assetInfo.data) || {};
       setConnectedAssetData(constructElementDetailsObject(element, details));
+      showConnectedAssets();
     }
-  }, [elemIsAsset, assetInfo.data, element, setConnectedAssetData]);
-
+  };
   if (isLoading) {
     return (
       <Box display="flex" justifyContent="center" mt={2}>
@@ -118,7 +117,9 @@ export default function ElementDetails({
               display="flex"
               alignItems="center"
               sx={{ cursor: "pointer", mb: 1, whiteSpace: "nowrap" }}
-              onClick={() => showConnectedAssets()}
+              onClick={() => {
+                onClick();
+              }}
             >
               <Typography variant="body2" sx={{ fontWeight: 500 }}>
                 View connected assets
@@ -156,7 +157,6 @@ export default function ElementDetails({
 }
 
 function constructElementDetailsObject(element: any, details: any) {
-  console.log("constructElementDetailsObject", element);
   return {
     dependent: element?.dependent || {},
     assetUri: element?.uri || "",
