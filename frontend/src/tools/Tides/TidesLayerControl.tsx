53dc66f9329da5f10833865898cd48841f18a5d0
warraicha <abdullah.warraich@informed.com>
2025-03-24 13:06:08 +0000

    fix(DPAV-655): search results in layers menu

--- frontend/src/tools/Tides/TidesLayerControl.tsx
+++ frontend/src/tools/Tides/TidesLayerControl.tsx
@@ -5,6 +5,13 @@ import type { LayerControlProps } from "@/tools/Tool";
 export default function TidesLayerControl({
   searchQuery,
 }: Readonly<LayerControlProps>) {
+  if (searchQuery &&
+    !("Tides".toLowerCase().includes(searchQuery.toLowerCase()) ||
+      "Tidal".toLowerCase().includes(searchQuery.toLowerCase()) ||
+      "monitoring".toLowerCase().includes(searchQuery.toLowerCase()))) {
+  return null;
+  }
+
   return (
     <ComplexLayerControl title="Tides">
       <SimpleLayerControl
