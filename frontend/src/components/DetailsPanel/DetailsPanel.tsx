c5461b7e8e9af44db007a1f943e635ab14616e26
DavidBuckley-Informed <david.buckley@informed.com>
2025-03-25 13:11:10 +0000

    Merge branch 'develop' into feature/DPAV-354-Redesign-left-menu-search

--- frontend/src/components/DetailsPanel/DetailsPanel.tsx
+++ frontend/src/components/DetailsPanel/DetailsPanel.tsx
@@ -15,57 +15,12 @@ export default function DetailsPanel({
   onClose,
   isOpen,
 }: DetailsPanelProps) {
-  const [height, setHeight] = useState(300);
-  const panelRef = useRef<HTMLDivElement>(null);
-  const resizeRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const resizeHandle = resizeRef.current;
-    if (!resizeHandle) {
-      return;
-    }
-
-    let startY: number;
-    let startHeight: number;
-
-    const onMouseMove = (e: MouseEvent) => {
-      const deltaY = startY - e.clientY;
-      const newHeight = Math.max(
-        100,
-        Math.min(startHeight + deltaY, window.innerHeight - 100),
-      );
-      setHeight(newHeight);
-    };
-
-    const onMouseUp = () => {
-      document.body.classList.remove(styles.noSelect);
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-    };
-
-    const onMouseDown = (e: MouseEvent) => {
-      document.body.classList.add(styles.noSelect);
-      startY = e.clientY;
-      startHeight = height;
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    };
-
-    resizeHandle.addEventListener("mousedown", onMouseDown);
-
-    return () => {
-      resizeHandle.removeEventListener("mousedown", onMouseDown);
-    };
-  }, [height]);
-
   return (
     <div
-      ref={panelRef}
       className={classNames(styles.detailsPanel)}
       data-expanded={isOpen}
-      style={{ height: `${height}px`, maxHeight: "22vh" }}
+      style={{ height: `350px`, maxHeight: "22vh" }}
     >
-      <div ref={resizeRef} className={styles.resizeHandle} />
       {onClose && (
         <button onClick={onClose} className={styles.toggleButton}>
           <FontAwesomeIcon icon={faChevronDown} size="2x" />
