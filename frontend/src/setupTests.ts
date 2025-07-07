4a84b7a37dbfff2b11864c880f76d606773e0da9
Jordon Smith <jordon.smith@informed.com>
2025-07-07 09:49:24 +0100

    chore(NON-REQ): work around "Cannot use 'import.meta' outside a module"

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom";
+import { jest } from "@jest/globals";
+
+// mockout featureFlags to avoid import.meta issues without babel transforms
+jest.mock("./config/feature-flags", () => ({
+  default: {
+    devTools: true,
+    routing: true,
+    uiNext: true,
+    pageHeader: false,
+    feedbackWidget: false,
+    environmentallySensitiveAreas: true,
+    assetTable: true,
+  },
+}));
