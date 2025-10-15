d88196f3cb5160326d8428111002da451d9f35ee
kingarj <alice.king@informed.com>
2025-10-15 17:05:33 +0100

    ruff

--- backend/vista-python-api/core/core/tests/test_project.py
+++ backend/vista-python-api/core/core/tests/test_project.py
@@ -2,23 +2,21 @@
 
 from __future__ import annotations
 
+from core import gunicorn
+from core.asgi import asgi_application
+from core.wsgi import wsgi_application
+
 
 def test_wsgi():
     """Import the wsgi app just to get coverage."""
-    from core.wsgi import application
-
-    assert application is not None
+    assert wsgi_application is not None
 
 
 def test_asgi():
     """Import the asgi app just to get coverage."""
-    from core.asgi import application
-
-    assert application is not None
+    assert asgi_application is not None
 
 
 def test_gunicorn():
     """Import the gunicorn config just to get coverage."""
-    from core import gunicorn
-
     assert gunicorn is not None
