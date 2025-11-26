2c65a5731ebf439f5ebe0cd148a7fae1ea4f999e
Alice King <30341523+kingarj@users.noreply.github.com>
2025-11-26 14:19:26 +0000

    feature: [DPAV-2156] add dependency and provider information into asset details endpoint (#513)

--- backend/vista-python-api/src/api/views/assets.py
+++ backend/vista-python-api/src/api/views/assets.py
@@ -4,20 +4,29 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 
 from api.models.asset import Asset
-from api.serializers import AssetSerializer
+from api.serializers import AssetDetailSerializer, AssetListSerializer
 
 
 class AssetViewSet(viewsets.ReadOnlyModelViewSet):
     """A ModelViewSet for the Asset model."""
 
     queryset = Asset.objects.all()
-    serializer_class = AssetSerializer
+
+    def get_serializer_class(self):
+        """Get serializer class for action."""
+        if self.action == "list":
+            return AssetListSerializer
+        return AssetDetailSerializer
 
     def get_queryset(self):
         """Get queryset in deference to request query parameters."""
         asset_type_id = self.request.query_params.get("asset_type", None)
         if asset_type_id is not None:
             self.queryset = self.queryset.filter(type=asset_type_id)
+
+        if self.action == "retrieve":
+            # Prefetch reverse relationships only for the detail view
+            return Asset.objects.prefetch_related("provider", "dependent")
         return super().get_queryset()
 
     @action(detail=False, methods=["get"], url_path="")
