35335d8f8ede7da2339cb01a7ae809de0b246082
Alice King <30341523+kingarj@users.noreply.github.com>
2025-12-12 12:11:41 +0000

    feature: [DPAV-2130] allow update of scenario (#542)
    * feature: [DPAV-2130] allow update of scenario
    
    * pr feedback

--- backend/vista-python-api/src/api/views/scenarios.py
+++ backend/vista-python-api/src/api/views/scenarios.py
@@ -1,13 +1,16 @@
 """Views for Scenarios."""
 
+from typing import ClassVar
+
 from rest_framework import viewsets
 
 from api.models import Scenario
 from api.serializers import ScenarioSerializer
 
 
-class ScenarioViewSet(viewsets.ReadOnlyModelViewSet):
+class ScenarioViewSet(viewsets.ModelViewSet):
     """ViewSet for Scenario read operations."""
 
+    http_method_names: ClassVar = ["get", "post", "patch"]
     queryset = Scenario.objects.all()
     serializer_class = ScenarioSerializer
