8c405bc4ffc7138c1ca91162d17c2fae4cc1a9c9
Alice King <30341523+kingarj@users.noreply.github.com>
2026-01-07 13:04:40 +0000

    bugfix: [DPAV-2261] allow HEAD verb (#580)

--- backend/vista-python-api/src/core/views.py
+++ backend/vista-python-api/src/core/views.py
@@ -1,10 +1,10 @@
 """Core views for system use."""
 
 from django.http import HttpResponse
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_http_methods
 
 
-@require_GET
+@require_http_methods(["GET", "HEAD"])
 def ping(_request):
     """Confirm that we are up and that the user has been able to access us."""
     return HttpResponse("OK", content_type="text/plain")
