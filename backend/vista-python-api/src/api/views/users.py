2e625b5d7cf7fbc4dc9adf8e8986359a172c55ff
Alice King <30341523+kingarj@users.noreply.github.com>
2026-02-09 12:06:07 +0000

    feature: [DPAV-2462] pending invite endpoint (#639)

--- backend/vista-python-api/src/api/views/users.py
+++ backend/vista-python-api/src/api/views/users.py
@@ -6,7 +6,7 @@ from datetime import UTC, datetime, timedelta
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from api.models.group import GroupMembership
 from api.models.user_invite import UserInvite
@@ -16,7 +16,7 @@ from api.serializers import IdpUserSerializer, UserCreateSerializer, UserInviteS
 from api.utils.auth import get_user_id_from_request
 
 
-class ApplicationUserViewSet(ViewSet):
+class ApplicationUserViewSet(ModelViewSet):
     """Views for interfacing with application users."""
 
     logger = logging.getLogger(__name__)
@@ -29,6 +29,19 @@ class ApplicationUserViewSet(ViewSet):
     def get_permissions(self):
         """Get permissions for viewset methods."""
         return [Administrator()]
+
+    def get_serializer_class(self):
+        """Fetch the correct response serializer for endpoint."""
+        if self.action in ["list_invites", "create"]:
+            return UserInviteSerializer
+        return IdpUserSerializer
+
+    def get_serializer_context(self):
+        """Get external user context for serializer."""
+        context = super().get_serializer_context()
+        idp_users = self.idp_repository.list_users_in_group()
+        context["idp_user_map"] = {idp_user.id: idp_user for idp_user in idp_users}
+        return context
 
     def list(self, _request):
         """Return a list of all users."""
@@ -47,15 +60,20 @@ class ApplicationUserViewSet(ViewSet):
         group_ids = serializer.validated_data.get("group_ids")
         user_id = self.idp_repository.create_user(email, user_type == "admin")
 
+        existing_user_invite = UserInvite.objects.filter(user_id=user_id)
+        if len(existing_user_invite) > 0:
+            return Response(
+                {"detail": "Account already exists for user."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user_invite = UserInvite.objects.create(
             user_id=user_id, status="pending", created_by=req_user_id
         )
         self._create_group_memberships(group_ids, user_id, req_user_id)
 
-        return Response(
-            UserInviteSerializer(user_invite).data,
-            status=status.HTTP_201_CREATED,
-        )
+        serializer = self.get_serializer(user_invite)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, pk):  # noqa: ARG002
         """Delete user from vista Cognito groups and user-associated entities."""
@@ -71,6 +89,15 @@ class ApplicationUserViewSet(ViewSet):
         self._check_accept_current_user_invite(req_user_id)
         self._check_all_pending_invites_for_expiry()
         return Response(status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"], url_path="pending-invites")
+    def list_invites(self, request):  # noqa: ARG002
+        """Fetch any pending or expired user invites."""
+        user_invites = UserInvite.objects.filter(
+            status__in=[UserInvite.PENDING, UserInvite.EXPIRED]
+        )
+        serializer = self.get_serializer(user_invites, many=True)
+        return Response(serializer.data)
 
     def _check_accept_current_user_invite(self, user_id):
         try:
